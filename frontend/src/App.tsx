fc9a9a3e3bf81e9c349f357d00b1d8d6d8ba94ea
Your Name <you@example.com>
2025-05-05 11:56:10 +0530

    fix: change scripts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
 import ThemeCustomisation from './theme/theme'
-import Routing from './routing/routing'
-import { AuthProvider } from './UI/contexts/contexts'
+// import Routing from './routing/routing'
+// import { AuthProvider } from './UI/contexts/contexts'
 
-function App() {
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <ThemeCustomisation>
+//         <Routing />
+//       </ThemeCustomisation>
+//     </AuthProvider>
+//   )
+// }
+
+// export default App
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+export default () => {
   return (
-    <AuthProvider>
-      <ThemeCustomisation>
-        <Routing />
-      </ThemeCustomisation>
-    </AuthProvider>
-  )
-}
-
-export default App
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
